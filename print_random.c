3597639b87da8cee223a1a5d915c59a1d6d8afb8
Hacker-2004 <tup13859@temple.edu>
2024-08-30 12:20:45 -0400

    new changes

--- print_random.c
+++ print_random.c
@@ -3,53 +3,26 @@
 #include <stdio.h>
 #include <time.h>
 
-// Function for the 6 words
-void rand_string(char *str,size_t size){
 
-    // loops through each position in the string
- for (size_t i=0; i<size; i++){
-    // calls the randchar function from random.c and sets the empty string value to that random alphabet for the size i
-    str[i]=randchar();
- }
- // to mark the end of the string
- str[size] = '\0';
-}
-
-
-//Given Project code
-// this function takes the size to allocate the memory as the size
-
-char* rand_string_alloc(size_t size)
+// the main where the code is going to output the solution
+int main()
 {
-    // makes a memory for the char and then the +1 for empty string
-     char *s = malloc(size + 1);
+    // a variable for the for loop
+	int a;
 
-     // checks if the memory was created or not
-     if (s) {
-        // if created then it calls the rand_string function to get random char with the memory point and the size
-         rand_string(s, size);
-     }
-     // returns the empty point and returns to 0 to exit the code
-     return s;
-}
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
 
-int main(){
-    // the current time
-    srand(time(0));
+    // prints the random word
+	printf("Today's random word: ");
 
-    //sets the size of the string 
-    size_t size = 7;
+    // for loop to get the random word for 7 times
+	for(a=0;a<7;a++)
+		putchar( randchar() );
 
-    // the allocation memory function and to get the 7 char word
-    char *randword = rand_string_alloc(size);
+    // after the end of the for loop the \n as an empty string
+	putchar('\n');
 
-    // checks if the word is not empty and then prints the word
-    if(randword){
-        // it prints the word as a string
-        printf("Ramdom Word: %s\n",randword);
-        // frees the allocated memory
-        free(randword);
-    }
-    // returns 0 to exit the code
-    return 0;
+// exits the code.
+	return(0);
 }
--- random.h
+++ random.h
@@ -2,7 +2,7 @@
 // define a library as global so that the print_random can use the randchar since the function is in different file
 
 #ifndef RANDOM_H
-#define RAMDOM_H
+#define RANDOM_H
 
 // the function name and the type char
 char randchar();
